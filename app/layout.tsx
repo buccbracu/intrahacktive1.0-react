682da5d716505130490d0e653776e19792a1124a
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2024-10-22 04:43:37 +0600

    Update bootstrap CDN link in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+import Head from "next/head";
 import { Toaster } from "sonner";
 import BootstrapClient from "./components/BootstrapClient";
 import Footer from "./components/Footer";
@@ -30,7 +31,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
+      <Head>
+        {" "}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        />{" "}
+      </Head>
       <body className={spaceGrotesk.className}>
         <NavBar />
         <Toaster position="bottom-right" richColors />
